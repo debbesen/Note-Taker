6c08b88c172964e5a566ef6deeb8ff7c620698f1
DuRyan Ebbesen <duryanebbesen@yahoo.com>
2020-12-13 10:55:24 -0800

    deploying

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/notetracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
